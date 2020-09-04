0bb3f2c6b60b7ce2174ff85f28f59cd0983be13e
Camilo Oscar Girardelli Baptista <girardellibaptista@gmail.com>
2020-09-04 09:46:40 -0300

    Initial Commit

---
+++ SDKPagamentoBR/SDKPagamentoBR.h
@@ -0,0 +1,19 @@
+//
+//  SDKPagamentoBR.h
+//  SDKPagamentoBR
+//
+//  Created by Camilo Oscar Girardelli Baptista on 04/09/20.
+//  Copyright © 2020 ClaroPay. All rights reserved.
+//
+
+#import <Foundation/Foundation.h>
+
+//! Project version number for SDKPagamentoBR.
+FOUNDATION_EXPORT double SDKPagamentoBRVersionNumber;
+
+//! Project version string for SDKPagamentoBR.
+FOUNDATION_EXPORT const unsigned char SDKPagamentoBRVersionString[];
+
+// In this header, you should import all the public headers of your framework using statements like #import <SDKPagamentoBR/PublicHeader.h>
+
+
